068dfcb9b57213809d678f38f476174998b1ca7a
Juanmaelgoat <153026255+Juanmaelgoat@users.noreply.github.com>
2023-12-06 02:25:19 -0500

    Add files via upload

---
+++ register.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+       
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+<style>.gradient-custom-2 {
+/* fallback for old browsers */
+background: #fccb90;
+
+/* Chrome 10-25, Safari 5.1-6 */
+background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+
+/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+}
+
+@media (min-width: 768px) {
+.gradient-form {
+height: 100vh !important;
+}
+}
+@media (min-width: 769px) {
+.gradient-custom-2 {
+border-top-right-radius: .3rem;
+border-bottom-right-radius: .3rem;
+}
+}</style>
+    <body>
+    <section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+
+                <div class="text-center">
+                  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Sena_Colombia_logo.svg/2090px-Sena_Colombia_logo.svg.png"
+                    style="width: 185px;" alt="logo">
+                    <h4 class="mt-1 mb-5 pb-1">LOGIN</h4>
+</div>
+
+<form action="{{ route('register') }}" method="post">
+    @csrf 
+    <p>Por favor, inicie sesión en su cuenta</p>
+
+    <div class="form-outline mb-4">
+        <label class="form-label" for="form2Example11">Nombre</label>
+        <input type="text" name="name" id="form2Example11" class="form-control" placeholder="Ingresar nombre" />
+    </div>
+
+    <div class="form-outline mb-4">
+        <label class="form-label" for="form2Example11">Correo</label>
+        <input type="email" name="email" id="form2Example11" class="form-control" placeholder="Ingresar dirección de correo electrónico" />
+    </div>
+
+    <div class="form-outline mb-4">
+        <label class="form-label" for="form2Example22">Contraseña</label>
+        <input type="password" name="password" id="form2Example22" class="form-control" />
+    </div>
+
+    <div class="form-outline mb-4">
+        <label class="form-label" for="form2Example22">Confirmar Contraseña</label>
+        <input type="password" name="password_confirmation" id="form2Example22" class="form-control" />
+    </div>
+
+    <div class="text-center pt-1 mb-5 pb-1">
+        <button class="bn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="submit">Ingresar</button>
+    </div>
+
+    <div class="d-flex align-items-center justify-content-center pb-4">
+        <p class="mb-0 me-2">Ir al login</p>
+        <a href="{{ route('login') }}" class="btn btn-outline-danger">Login</a>
+    </div>
+</form>
+
+ 
+  </div> 
+
+</form>
+
+</div>
+<div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+  <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
